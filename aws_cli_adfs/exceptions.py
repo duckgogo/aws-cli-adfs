3cfac524737f8caa924ef66f2ce3f57ba15bc16f
rickzeng <zengjx92@hotmail.com>
2019-03-21 17:55:28 +0800

    add several exceptions

--- aws_cli_adfs/exceptions.py
+++ aws_cli_adfs/exceptions.py
@@ -17,3 +17,25 @@ class LoginNotApprovedException(Exception):
 
     def __init__(self, *args, **kwargs):
         super(LoginNotApprovedException, self).__init__(*args, **kwargs)
+
+
+class WrongIDPEntryUrlException(Exception):
+    """Wrong IDP entry url"""
+
+    def __init__(self, *args, **kwargs):
+        super(WrongIDPEntryUrlException, self).__init__(*args, **kwargs)
+
+
+class WrongRelyingPartyException(Exception):
+    """Wrong relying party"""
+
+    def __init__(self, *args, **kwargs):
+        super(WrongRelyingPartyException, self).__init__(*args, **kwargs)
+
+
+class WrongAWSRegionException(Exception):
+    """Wrong AWS region"""
+
+    def __init__(self, region, *args, **kwargs):
+        self.region = region
+        super(WrongAWSRegionException, self).__init__(*args, **kwargs)
